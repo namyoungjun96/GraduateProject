e416452b44dfbab063f5c84957e45adf120c8acc
namyoungjun96 <ehdgornltls@naver.com>
2021-09-25 20:00:12 +0900

    add videoStream.js

---
+++ videoStream.js
@@ -0,0 +1,104 @@
+let lastFrameObj = {
+    lastFrame: null
+};
+
+let videoStream = (function(){
+    var raspberryPiCamera;
+
+    return {
+        getLastFrame: () => {
+            return lastFrameObj.lastFrame;
+        },
+        acceptConnections: function(expressApp, cameraOptions, resourcePath, isVerbose){
+            raspberryPiCamera = require('raspberry-pi-camera-native');
+    
+            if(!cameraOptions){
+                cameraOptions = {
+                    width: 1280,
+                    height: 720,
+                    fps: 16,
+                    encoding: 'JPEG',
+                    quality: 7
+                };
+            }
+    
+            // start capture
+            raspberryPiCamera.start(cameraOptions);
+            if(isVerbose) {
+                console.log('Camera started.');
+            }
+    
+            if(typeof resourcePath === 'undefined' || !resourcePath){
+                resourcePath = '/stream.mjpg';
+            }
+    
+            expressApp.get(resourcePath, (req, res) => {
+    
+                res.writeHead(200, {
+                    'Cache-Control': 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0',
+                    Pragma: 'no-cache',
+                    Connection: 'close',
+                    'Content-Type': 'multipart/x-mixed-replace; boundary=--myboundary'
+                });
+                if(isVerbose)
+                    console.log('Accepting connection: '+req.hostname);
+    
+                // add frame data event listener
+            
+                let isReady = true;
+            
+                let frameHandler = (frameData) => {
+                    try{
+                        if(!isReady){
+                            return;
+                        }
+            
+                        isReady = false;
+        
+                        if(isVerbose)
+                            console.log('Writing frame: '+frameData.length);
+    
+                        lastFrameObj.lastFrame = frameData;
+    
+                        res.write(`--myboundary\nContent-Type: image/jpg\nContent-length: ${frameData.length}\n\n`);
+                        res.write(frameData, function(){
+                            isReady = true;
+                        });
+            
+            
+                    }
+                    catch(ex){
+                        if(isVerbose)
+                            console.log('Unable to send frame: '+ex);
+                    }
+                }
+            
+                let frameEmitter = raspberryPiCamera.on('frame', frameHandler);
+            
+                req.on('close', ()=>{
+                    frameEmitter.removeListener('frame', frameHandler);
+    
+                    if(isVerbose)
+                        console.log('Connection terminated: '+req.hostname);
+                });
+            });
+        },
+        pauseCamera: function(){
+            raspberryPiCamera.pause(function(){
+                console.log("streaming pause");
+            });
+        },
+        resumeCamera: function(){
+            raspberryPiCamera.resume(function(){
+                console.log("streaming resume");
+            });
+        },
+        stopCamera: function(){
+            delete require.cache[name];
+        }
+    }
+})();
+    
+
+
+module.exports = videoStream;
